8dc0d6e9c6880d11501681032fc82ffa81f0abcb
N Altman <altmann@dewv.edu>
2018-09-04 16:57:18 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,1 +1,17 @@
+import {game, Sprite} from "./sgc/sgc.js";
 
+game.setBackground("floor.png");
+
+class PlayerWizard extends Sprite{
+    constructor() {
+        super();
+        this.name = "Marcus the Wizard";
+        this.setimage("marcusSheet.png");
+        this.width = 48;
+        this.height = 48;
+        this.x = this.width;
+        this.y = this.y;
+    }
+}
+
+let marcus = new PlayerWizard();
