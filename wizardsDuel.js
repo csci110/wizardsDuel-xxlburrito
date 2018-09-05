5c3e4269c766698a34ab9dda7222f10e76617842
N Altman <altmann@dewv.edu>
2018-09-05 16:50:32 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -2,19 +2,62 @@ import {game, Sprite} from "./sgc/sgc.js";
 
 game.setBackground("floor.png");
 
+class Spell extends Sprite{
+    constructor() {
+        super();
+        this.speed = 200;
+        this.height = 48;
+        this.width = 48;
+        this.defineAnimation("magic", 0, 7);
+    }
+    handleBoundaryContact() {
+        // delete spell when it leaves display area
+        game.removeSprite(this);
+    }
+}
+
 class PlayerWizard extends Sprite{
     constructor() {
         super();
         this.name = "Marcus the Wizard";
-        this.setimage("marcusSheet.png");
+        this.setImage("marcusSheet.png");
         this.width = 48;
         this.height = 48;
         this.x = this.width;
         this.y = this.y;
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("up", 0, 2);
+        this.defineAnimation("right", 3, 5);
+        this.speedWhenWalking = 100;
+        
+    }
+    handleDownArrowKey() {
+        this.playAnimation("down");
+        this.speed = this.speedWhenWalking;
+        this.angle = 270;
+    }
+    handleUpArrowKey() {
+        this.playAnimation("up");
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
+    }
+    handleGameLoop() {
+        this.y = Math.max(0, this.y); // Keep Marcus in the display area
+        this.y = Math.min(552, this.y);
+    }
+    handleSpaceBar() {
+        let spell = new Spell();
+        spell.x = this.x; // this sets the position of the spell object equal to 
+        spell.y = this.y; // the position of any object created from the PlayerWizard class
+        spell.name = "A spell cast by Marcus";
+        spell.setImage("marcusSpellSheet.png");
+        spell.angle = 0;
+        spell.playAnimation("magic", true);
+        spell.playAnimation("right", false);
     }
 }
 
 let marcus = new PlayerWizard();
 
-// is there supposed to be a loader.js for this game?
+
 
